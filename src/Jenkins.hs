8b8f6b07380352fc93403f4e9fcdc01c8e364b47
wayofthepie <wayofthepie@gmail.com>
2014-12-14 06:46:30 +0000

    Add types to functions

--- src/Jenkins.hs
+++ src/Jenkins.hs
@@ -1,13 +1,16 @@
-{-# LANGUAGE Arrows, NoMonomorphismRestriction #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
 module Jenkins where
 
 import Control.Lens hiding (deep)           -- lens
 import Control.Monad
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Control
 import Data.Aeson.Lens                      -- lens-aeson
 import Data.ByteString.Lazy.Internal
 import Data.Map (empty)
+import Data.Text hiding (empty)
 import Jenkins.Rest (Jenkins, (-?-), (-=-)) -- libjenkins
 import qualified Jenkins.Rest as JR
 import Text.Hamlet.XML
@@ -19,6 +22,7 @@ master :: JR.Master
 master = JR.defaultMaster &
     JR.url .~ "http://192.168.59.103:8080"
 
+config :: Element
 config = Element "project" empty [xml|
     <actions>
     <description>
@@ -41,7 +45,7 @@ config = Element "project" empty [xml|
     <buildWrappers>
 |]
 
-
+createJob :: ( MonadBaseControl IO m, MonadIO m ) => Text -> m ( JR.Result () )
 createJob n = 
     JR.run (JR.defaultMaster &
         JR.url .~ ("http://192.168.59.103:8080/")) $
