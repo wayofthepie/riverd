2cb7df9cd18cb89109f54f4403f36ba4bcc57979
wayofthepie <wayofthepie@gmail.com>
2014-12-14 06:35:30 +0000

    Add basic job creation functions

--- src/Jenkins.hs
+++ src/Jenkins.hs
@@ -1,15 +1,18 @@
 {-# LANGUAGE Arrows, NoMonomorphismRestriction #-}
 {-# LANGUAGE OverloadedStrings #-}
-
+{-# LANGUAGE QuasiQuotes #-}
 module Jenkins where
 
 import Control.Lens hiding (deep)           -- lens
 import Control.Monad
 import Data.Aeson.Lens                      -- lens-aeson
 import Data.ByteString.Lazy.Internal
+import Data.Map (empty)
 import Jenkins.Rest (Jenkins, (-?-), (-=-)) -- libjenkins
 import qualified Jenkins.Rest as JR
+import Text.Hamlet.XML
 import Text.XML.HXT.Core 
+import Text.XML
 
 -- Hardcode the master for now
 master :: JR.Master
@@ -99,3 +102,36 @@ bcResult = do
     case result of
         []  -> putStrLn "Error parsing..."
         w:_ -> print w-}
+
+
+config = Element "project" empty [xml|
+    <actions>
+    <description>
+    <keepDependencies>
+        false
+    <properties>
+    <scm class="hudson.scm.NullSCM">
+    <canRoam>true
+    <disabled> false
+    <blockBuildWhenDownstreamBuilding>false
+    <blockBuildWhenUpstreamBuilding>false
+    <triggers>
+    <concurrentBuild>
+        false
+    <builders>
+        <hudson.tasks.Shell>
+            <command>
+                echo "test"
+    <publishers>
+    <buildWrappers>
+|]
+
+
+createJob n = 
+    JR.run (JR.defaultMaster &
+        JR.url .~ ("http://192.168.59.103:8080/")) $
+            JR.postXml ("createItem" -?- "name" -=- n) configData
+    where
+        configData = renderLBS def $ Document (Prologue [] Nothing []) config []
+
+
