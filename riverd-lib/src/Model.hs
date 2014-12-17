c3f0eef67c207a9f5f5920d0f619a07b3afe50e7
wayofthepie <wayofthepie@gmail.com>
2014-12-17 11:14:36 +0000

    Add work on persistent
    Pretty much just some hacking right now...

---
+++ riverd-lib/src/Model.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Model where
+
+import Control.Monad.IO.Class
+import Control.Applicative
+import Data.Text
+import Database.Persist
+import Database.Persist.Sqlite
+import Database.Persist.TH
+{-
+data Project = Project
+    { projectName :: !Text
+    , repoUrl     :: !Text
+    , jenkinsInfo :: !Text
+    } deriving (Eq, Show)
+
+
+data JenkinsInfo = JenkinsInfo
+    { url   :: !Text  -- | Base url of jenkins
+    , jobs  :: [Text] -- | List of job urls
+    } deriving (Eq, Show)
+-}
+
+share [mkPersist sqlSettings, mkSave "entityDefs"] [persistLowerCase|
+Project
+    projectName Text
+    repoUrl     Text
+    jenkinsInfo Text
+    deriving Show
+|]
+
+main :: IO ()
+main = runSqlite ":memory:" $ do
+    -- needed to create initial table
+    runMigration $ migrate entityDefs $ entityDef (Nothing :: Maybe Project)
+    testId  <- insert $ Project "Test" "github.com" "10.64.46.1"
+    test    <- get testId
+    liftIO $ print test
