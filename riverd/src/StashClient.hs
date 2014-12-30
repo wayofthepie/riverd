13d68ac936071b725f8ccd1686f56ba6978931ea
wayofthepie <wayofthepie@gmail.com>
2014-12-30 04:16:25 +0000

    Add documentation

--- riverd/src/StashClient.hs
+++ riverd/src/StashClient.hs
@@ -3,6 +3,11 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE OverloadedStrings #-}
 
+
+{-|
+    Module      : StashClient
+    Description : A REST client library for Atlassian Stash.
+-}
 module StashClient where
 
 import Control.Monad
@@ -26,6 +31,12 @@ import qualified Stash.Types.Project        as P
 import qualified Stash.Types.Repo           as R
 
 
+--------------------------------------------------------------------------------
+-- * Data Types
+
+-- ** StashClientConfig
+
+-- | Holds configuration info for the client
 data StashClientConfig = StashClientConfig
     { apiBase       :: String
     , apiVersion    :: String
@@ -35,7 +46,11 @@ data StashClientConfig = StashClientConfig
     , scManager     :: Manager
     }
 
-
+-- | Default StashClientConfig.
+--
+-- Takes a base URL (which includes the port) a username and  a password.
+-- Configures the protocol to be "http://" the REST API version to be "1.0" and
+-- uses a default manager (see withManager in Network.HTTP.Conduit).
 defaultStashClientConfig :: ( MonadBaseControl IO m,  MonadIO m ) =>
     String -> String -> String -> m StashClientConfig
 defaultStashClientConfig base user pass  =
@@ -49,18 +64,82 @@ defaultStashClientConfig base user pass  =
                                  }
 
 
+-- ** StashClient
+
+-- | ReaderT monad to allow us pass configuration around.
 type StashClient a = ReaderT StashClientConfig (ResourceT IO) a
 
 
--- | Example
+-- | Run a StashClient action with the given configuration.
 --
+-- Example:
 -- @
---  liftM (\\pr -> case pr of { Right x -> PR.values x; Left _ -> error ""})
---      runStashClient cfg $ getProjects
+--  defaultStashClientConfig "102.168.1.1:7990" "test" "test" >>=
+--      flip runStashClient (getRepo "TEST" "test")
 -- @
 runStashClient :: MonadIO m => StashClientConfig -> StashClient a -> m a
 runStashClient config action =
     liftIO $ runResourceT $ runReaderT action config
+
+
+
+--------------------------------------------------------------------------------
+-- * Building Blocks
+-- Helper functions to allow building higher level API request functions.
+
+reqEP :: String -> Maybe String ->
+    (Request -> Request) -> StashClient ( Response BLC.ByteString )
+reqEP ep q httpMethod = do
+    config <- ask
+    getResponse (scManager config) =<< requestBuilder httpMethod (endpoint config ep q)
+            (username config, password config)
+    where
+        endpoint :: StashClientConfig -> String -> Maybe String -> String
+        endpoint config ep q = apiEndpointUrl config ep q
+
+
+mod2get :: (Request -> Request)
+mod2get = \r -> r { method = "GET" }
+
+
+--------------------------------------------------------------------------------
+-- * Get Actions
+
+-- | Get a paged list of projects.
+--
+-- @
+--  defaultStashClientConfig "102.168.1.1:7990" "test" "test" >>=
+--      flip runStashClient getProjects
+-- @
+getProjects :: StashClient ( Either String ( PR.PagedResponse [P.Project] ) )
+getProjects = reqEP "projects" Nothing mod2get >>= return . eitherDecode . responseBody
+
+
+-- | Get a paged (defaults to 25 repos per request) list of repos in the given
+-- project.
+--
+-- @
+--  defaultStashClientConfig "102.168.1.1:7990" "test" "test" >>=
+--      flip runStashClient (getRepos "TEST")
+-- @
+getRepos :: String -> StashClient ( Either String ( PR.PagedResponse [R.Repo] ) )
+getRepos pkey = reqEP ep Nothing mod2get >>= return . eitherDecode . responseBody
+    where ep = "projects" ++ "/" ++ pkey ++ "/" ++ "repos"
+
+
+-- | Get a repo with the given name __rkey__ in the given project __pkey__.
+--
+-- @
+--  defaultStashClientConfig "102.168.1.1:7990" "test" "test" >>=
+--      flip runStashClient (getRepo "TEST" "test")
+-- @
+getRepo :: String -> String -> StashClient ( Either String R.Repo )
+getRepo pkey rkey = reqEP ep Nothing mod2get >>= return . eitherDecode . responseBody
+    where ep = "projects" ++ "/" ++ pkey ++ "/" ++ "repos" ++ "/" ++ rkey
+
+
+--------------------------------------------------------------------------------
+-- * Internal Functions
 
 
 -- | apiEndpointUrl hostname apiVersion query path
@@ -92,31 +171,3 @@ getResponse :: ( MonadIO m, MonadBaseControl IO m ) =>
     Manager -> Request -> m ( Response BLC.ByteString )
 getResponse manager req = httpLbs req manager
 
-
-reqEP :: String -> Maybe String ->
-    (Request -> Request) -> StashClient ( Response BLC.ByteString )
-reqEP ep q httpMethod = do
-    config <- ask
-    getResponse (scManager config) =<< requestBuilder httpMethod (endpoint config ep q)
-            (username config, password config)
-    where
-        endpoint :: StashClientConfig -> String -> Maybe String -> String
-        endpoint config ep q = apiEndpointUrl config ep q
-
-
-mod2get :: (Request -> Request)
-mod2get = \r -> r { method = "GET" }
-
-
-getProjects :: StashClient ( Either String ( PR.PagedResponse [P.Project] ) )
-getProjects = reqEP "projects" Nothing mod2get >>= return . eitherDecode . responseBody
-
-
-getRepos :: String -> StashClient ( Either String ( PR.PagedResponse [R.Repo] ) )
-getRepos pkey = reqEP ep Nothing mod2get >>= return . eitherDecode . responseBody
-    where ep = "projects" ++ "/" ++ pkey ++ "/" ++ "repos"
-
-
-getRepo :: String -> String -> StashClient ( Either String R.Repo )
-getRepo pkey rkey = reqEP ep Nothing mod2get >>= return . eitherDecode . responseBody
-    where ep = "projects" ++ "/" ++ pkey ++ "/" ++ "repos" ++ "/" ++ rkey
