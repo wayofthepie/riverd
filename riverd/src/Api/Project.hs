db287ac65601230d162aa780e24a5275f66654a0
wayofthepie <wayofthepie@gmail.com>
2015-01-10 23:25:51 +0000

    Add changes to allow pickling of lists
    Adds the instance for JSONSchema and XmlPickler instances for a [String]

--- riverd/src/Api/Project.hs
+++ riverd/src/Api/Project.hs
@@ -5,12 +5,15 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies #-}
 
-module Api.Project (resource) where
+module Api.Project where
 
+import Control.Monad.Error (ErrorT)
 import Control.Monad.Reader
 import Data.Aeson
-import Data.JSON.Schema hiding (content)
-import Data.Text (Text)
+import Data.JSON.Schema as JS hiding (content)
+import Data.JSON.Schema.Combinators
+    (addField, array, empty, field, merge, value, (<+>))
+import Data.Text (pack,unpack,Text)
 import Data.Typeable
 import GHC.Generics
 import Generics.Generic.Aeson
@@ -18,13 +21,12 @@ import Generics.Regular
 import Generics.Regular.XmlPickler
 import Rest
 import qualified Rest.Resource as R
-import Text.XML.HXT.Arrow.Pickle
+import Text.XML.HXT.Arrow.Pickle hiding (Schema)
 
-type Title = String
 
 data Project = Project
-    { title :: Title
-    , content :: String
+    { title :: String --Text
+    , content :: [String]--[Text]
     } deriving (Generic, Typeable, Show)
 
 deriveAll ''Project "PFProject"
@@ -32,10 +34,23 @@ deriveAll ''Project "PFProject"
 type instance PF Project = PFProject
 instance FromJSON   Project where parseJSON = gparseJson
 instance ToJSON     Project where toJSON    = gtoJson
-instance JSONSchema Project where schema    = gSchema
-instance XmlPickler Project where xpickle   = gxpickle
 
-resource :: Resource IO (ReaderT Title IO) Title () Void
+instance JSONSchema Project where
+    schema  _ =
+        let contentF    = field "content" False (JS.Array unboundedLength False value)
+            titleF      = field "title" True value
+        in merge contentF titleF
+--        addField "title" True (value) $ field "content" False (JS.Array unboundedLength False value)
+
+instance XmlPickler Project where
+    xpickle = xpElem "project" $
+        xpWrap ( (\(a, bs) -> Project a bs), (\(Project a bs) -> (a, bs) ) ) $
+            xpPair (xpElem "title" xpText) (xpElem "list" $ xpList (xpElem "item" xpText))
+
+
+
+
+resource :: Resource IO (ReaderT String IO) String () Void
 resource = mkResourceReader
     { R.name    = "project"
     , R.schema  = withListing () $ named [("title", singleBy id)]
@@ -43,20 +58,25 @@ resource = mkResourceReader
     , R.get     = Just getProject
     }
 
-getProject :: Handler (ReaderT Title IO)
+getProject :: Handler (ReaderT String IO)
 getProject = mkIdHandler xmlJsonO $ \_ title -> liftIO $ readProject title
 
-readProject :: Title -> IO Project
-readProject t = return $ Project { title = t, content = "Hey!" }
+readProject :: String -> IO Project
+readProject t = return $ Project { title = t, content = ["Test!", "Hey!"] }
 
 listProjects :: ListHandler IO
-listProjects = mkListing xmlJsonO $ \range -> lift $ readProjects (offset range) (count range)
+listProjects = mkListing xmlJsonO handler
+    where
+        handler :: Range -> ErrorT (Reason ()) IO [Project]
+        handler r = do
+            liftIO $ readProjects (offset r) (count r)
+
 
 readProjects :: Int -> Int -> IO [Project]
-readProjects _ _ = return
-    [
-        Project { title = "Project One", content = "First." },
-        Project { title = "Project Two", content = "Second!" }
-    ]
-
+readProjects _ _ = do
+    return $
+        [
+            Project { title = "Project One", content = ["First."] },
+            Project { title = "Project Two", content = ["Second!"] }
+        ]
 
