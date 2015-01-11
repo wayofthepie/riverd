9a4c0ab6f5fe8dfedc8995fa0b52bae8c0cbfcb8
wayofthepie <wayofthepie@gmail.com>
2015-01-11 10:18:31 +0000

    Add module for xml file gen from types

---
+++ riverd/src/lib/Model/XmlGen.hs
@@ -0,0 +1,28 @@
+
+-- | Convenience functions to view xml structure of types,
+-- these should be moved to test....
+module Model.XmlGen where
+
+import Model.Repository
+import Text.XML.HXT.DOM.TypeDefs
+import Text.XML.HXT.Arrow.Pickle.Xml
+import Text.XML.HXT.Arrow.Pickle
+import Control.Arrow
+import Control.Arrow.ArrowList
+import Text.XML.HXT.Arrow.Edit
+import Text.XML.HXT.Arrow.XmlState
+
+genXmlFile :: a -> PU a -> String -> IO [XmlTree]
+genXmlFile togen pickler fileName =
+    runX ( constA togen >>> xpickleDocument pickler [withIndent yes] fileName)
+
+
+exampleProject =
+    Project "test"
+        [ExternalDependency "gradle" "2.2.0" "Gradle!"]
+        ["gradle clean build"]
+
+
+testGenFromProject :: IO [XmlTree]
+testGenFromProject =
+    genXmlFile exampleProject (xpickle::PU Project) "test.xml"
